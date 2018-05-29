3e6e310fa87ec00196693645d82043c4d51a59b0
Ayman Salah <mo_ayman@live.com>
2018-05-29 03:00:06 +0200

    Improving CF 236B a bit

--- codeforces/codeforces_236B.cpp
+++ codeforces/codeforces_236B.cpp
@@ -2,65 +2,39 @@
 #include <cstring>
 using namespace std;
 
-int dp[1000001];
-int primes[1000];
-int numberOfPrimes;
-
-int SieveOfEratosthenes()
-{
-	bool prime[1001];
-	memset(prime, true, sizeof(prime));
-
-	for (int p = 2; p*p <= 1000; p++)
-	{
-		if (prime[p] == true)
-		{
-			for (int i = p * 2; i <= 1000; i += p)
-				prime[i] = false;
-		}
-	}
-
-	int pindex = 0;
-	for (int p = 2; p <= 1000; p++)
-	if (prime[p])
-		primes[pindex++] = p;
-	return pindex;
-}
-
-int getDivisors(int n)
-{
-	if (dp[n] != 0)
-		return dp[n];
-	int numberOfDivisors = 1;
-	for (size_t i = 0; i < numberOfPrimes; i++)
-	{
-		int currentDivisors = 0;
-		while (n % primes[i] == 0)
-		{
-			n /= primes[i];
-			currentDivisors++;
-		}
-		numberOfDivisors *= currentDivisors + 1;
-	}
-	return dp[n] = numberOfDivisors;
-}
+#define MOD 1073741824
+long long dp[1000001];
 
 int main()
 {
-	numberOfPrimes = SieveOfEratosthenes();
-	int a, b, c, answer = 0;
+	int a, b, c;
+	long long answer = 0;
 	cin >> a >> b >> c;
 	a++; b++; c++;
-	for (int i = 1; i < a; i++)
+	for (long long i = 1; i < a; i++)
 	{
-		for (int j = 1; j < b; j++)
+		for (long long j = 1; j < b; j++)
 		{
-			for (int k = 1; k < c; k++)
+			for (long long k = 1; k < c; k++)
 			{
-				answer += getDivisors(i*j*k) % 1073741824;
+				long long num = i*j*k;
+				if (dp[num] == 0)
+				{
+					long long numSqrt = sqrt((long double)num);
+					long long divisors = 2; // num itself and 1
+					for (long long d = 2; d <= numSqrt; d++)
+					{
+						if (d*d == num)
+							divisors++;
+						else if (num % d == 0)
+							divisors += 2; // the divisor and its counterpart 
+					}
+					dp[num] = divisors;
+				}
+				answer += dp[num] % MOD;
 			}
 		}
 	}
-	cout << answer % 1073741824;
+	cout << (answer % MOD) - 1;
 	return 0;
 }
